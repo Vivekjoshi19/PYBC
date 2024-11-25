9ca64e2a6f8ba09fb2d37f753d2376b7fc661f0b
sdac-9 <sdac-9@sdac-9-PC>
2024-11-24 19:02:04 -1200

    Emp

--- PBYC guit/src/EMPandManager/Emp.java
+++ PBYC guit/src/EMPandManager/Emp.java
@@ -1,5 +1,11 @@
 package EMPandManager;
 
-public class Emp {
+import Consumer.Consumer;
 
+public class Emp extends Consumer{
+ @Override
+public int CalSal(int sal) {
+	// TODO Auto-generated method stub
+	return sal;
+}
 }
